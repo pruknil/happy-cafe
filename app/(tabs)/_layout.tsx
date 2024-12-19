a5285d7913cca102e0a7313003cfed4c55e5dd03
pruknil <pruknil@gmail.com>
2024-12-19 15:31:32 +0700

    fix build

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,94 +1,85 @@
-import { HapticTab } from '@/components/HapticTab';
-import { useSession } from '@/components/ctx';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import {HapticTab} from '@/components/HapticTab';
+import {useSession} from '@/components/ctx';
+import {IconSymbol} from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Redirect, Tabs } from 'expo-router';
+import {Colors} from '@/constants/Colors';
+import {useColorScheme} from '@/hooks/useColorScheme';
+import {Redirect, Tabs} from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import {Platform} from 'react-native';
 // import { Text } from 'react-native';
 import {
-  MD3DarkTheme as DefaultTheme,
-  Text
+    MD3DarkTheme as DefaultTheme, Text
 } from 'react-native-paper';
+
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const { session, isLoading } = useSession();
-  const theme = {
-    ...DefaultTheme, // or MD3DarkTheme
-    roundness: 2,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: '#3498db',
-      secondary: '#f1c40f',
-      tertiary: '#a1b2c3',
-    },
-  };
-  // You can keep the splash screen open, or render a loading screen like we do here.
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
+    const colorScheme = useColorScheme();
+    const {session, isLoading} = useSession();
+    const theme = {
+        ...DefaultTheme, // or MD3DarkTheme
+        roundness: 2, colors: {
+            ...DefaultTheme.colors, primary: '#3498db', secondary: '#f1c40f', tertiary: '#a1b2c3',
+        },
+    };
+    // You can keep the splash screen open, or render a loading screen like we do here.
+    if (isLoading) {
+        return <Text>Loading...</Text>;
+    }
 
-  // Only require authentication within the (app) group's layout as users
-  // need to be able to access the (auth) group and sign in again.
-  if (!session) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
-    return <Redirect href="/sign-in" />;
-  }
+    // Only require authentication within the (app) group's layout as users
+    // need to be able to access the (auth) group and sign in again.
+    if (!session) {
+        // On web, static rendering will stop here as the user is not authenticated
+        // in the headless Node process that the pages are rendered in.
+        return <Redirect href="/sign-in"/>;
+    }
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+    return (<Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: false,
+                tabBarButton: HapticTab,
+                tabBarBackground: TabBarBackground,
+                tabBarStyle: Platform.select({
+                    ios: {
+                        // Use a transparent background on iOS to show the blur effect
+                        position: 'absolute',
+                    }, default: {},
+                }),
 
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="reward"
-        options={{
-          title: 'Reward',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gift.fill" color={color} />,
-        }}
-      />
+            }}>
             <Tabs.Screen
-        name="scan"
-        options={{
-          title: 'Scan',
-          tabBarIcon: ({ color}) => <IconSymbol size={60} name="viewfinder.circle.fill" color={color} style={{top:-15}} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="account"
-        options={{
-          title: 'Account',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+                name="index"
+                options={{
+                    title: 'Home', tabBarIcon: ({color}) => <IconSymbol size={28} name="house.fill" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="reward"
+                options={{
+                    title: 'Reward', tabBarIcon: ({color}) => <IconSymbol size={28} name="gift.fill" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="scan"
+                options={{
+                    title: 'Scan',
+                    tabBarIcon: ({color}) => <IconSymbol size={60} name="viewfinder.circle.fill" color={color}
+                                                         style={{top: -15}}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="mission"
+                options={{
+                    title: 'Mission',
+                    tabBarIcon: ({color}) => <IconSymbol size={28} name="target" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="account"
+                options={{
+                    title: 'Account', tabBarIcon: ({color}) => <IconSymbol size={28} name="person.fill" color={color}/>,
+                }}
+            />
+        </Tabs>);
 }
---
+++ app/(tabs)/mission.tsx
@@ -0,0 +1,35 @@
+import { Image, StyleSheet, View } from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import React from 'react';
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#E4D9F8', dark: '#AD8EE9' }}
+      headerImage={
+        <View style={{ flex: 1 }}>
+
+        </View>
+      }>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
